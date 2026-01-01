9b7477c202780481984dbd8511e44d0d84ef6ec3
Iskandar Rizki <iskandar.rizki@gmail.com>
2026-01-02 06:20:00 +0700

    add dotenvy for runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,7 @@
 import Config
+import Dotenvy
 
+source!([".env", System.get_env()])
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
